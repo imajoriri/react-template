004289081796bfb1c1394417dcbc6a3f5ab87d65
imajoriri <imajo.srroni@gmail.com>
2019-01-18 23:31:16 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import sample, { store } from "./modules/sample";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
